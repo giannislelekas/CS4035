ff4dd0bc5181caf47b11b42476459668793616b4
ilelekas <giannislelekas@gmail.com>
2019-06-21 18:36:52 +0200

    Added comments for Botnet profiling task

--- lab3/helper_functions/botnet_profiling_task.py
+++ lab3/helper_functions/botnet_profiling_task.py
@@ -2,6 +2,13 @@ import numpy as np
 import itertools
 import operator
 
+
+'''
+This function extract n-grams based on a sliding window of a given length.
+INPUT: data: the values to extract n-grams from,
+       n: length of the sliding window
+OUTPUT: ngrams: extracted n-grams
+'''
 def extract_ngrams(data, n):
     l = len(data)
     if l%n==0:
@@ -12,6 +19,15 @@ def extract_ngrams(data, n):
     return ngrams
 
 
+'''
+This function implements laplace smoothing, essentially adding combinations not present
+in a given profile. For the generation of all possible combinations the cartesian product is used.
+INPUT: unique_ngrams: given set of n-grams of a profile,
+       ngrams_counts: respective frequency of the n-grams
+       max_unigram: the max value encountered, used to generate all the possible combinations
+OUTPUT: smoothed_ngrams, smoothed_count: entire list of combinations with the added n-grams, appended
+                                        at the end with a count of 1.
+'''
 def laplace_smoothing(unique_ngrams, ngrams_counts, max_unigram):
 
 #     unigrams = np.unique(unique_ngrams)
@@ -36,6 +52,14 @@ def laplace_smoothing(unique_ngrams, ngrams_counts, max_unigram):
     return smoothed_ngrams, smoothed_count
 
 
+'''
+This function extracts a profile given a set of n-grams and their respective frequency.
+Essentially creates a dictionary for the profile with n-grams and their normalized frequency.
+INPUT: ngrams, ngrams_counts: n-grams and respective frequency
+OUTPUT: prof: profile with respective frequency,
+        prof_norm: profile with respective normalized frequency
+        sorted_count: sorted profile based on the normalized frequency
+'''
 def extract_profile(ngrams, ngrams_counts):
 
     ngrams = ngrams.astype(str)
@@ -50,6 +74,14 @@ def extract_profile(ngrams, ngrams_counts):
     return prof, prof_norm, np.array(sorted_count)
 
 
+'''
+This function implements the distance metric of the CNG method found in
+https://web.cs.dal.ca/~vlado/papers/pst04.pdf.
+INPUT: train_prof, host_prof: training and host profiles respectively,
+       sorted_train_prof, sorted_host_prof: respective sorted profiles based on
+                                            normalized frequency
+OUTPUT: dist: extracted distance
+'''
 def distance(train_prof, host_prof, sorted_train_prof, sorted_host_prof, N):
 
     if N>=len(sorted_train_prof):
@@ -78,3 +110,13 @@ def distance(train_prof, host_prof, sorted_train_prof, sorted_host_prof, N):
             f_host = host_prof[ngram]
         dist += ((f_train-f_host) / ((f_train+f_host)/2))**2
     return dist
+
+
+
+def performance_metrics(cm):
+    accuracy = (cm[0,0] + cm[1,1])/np.sum(cm)
+    precision = cm[0,0]/(cm[0,0]+cm[0,1])
+    recall = cm[0,0]/(cm[0,0]+cm[1,0])
+    F1_score = 2*(precision*recall)/(precision+recall)
+
+    return accuracy, precision, recall, F1_score
