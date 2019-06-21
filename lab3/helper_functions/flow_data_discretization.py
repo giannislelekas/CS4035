3b42bebe0770346fb16a2ba0af482d38fa003953
ilelekas <giannislelekas@gmail.com>
2019-06-21 17:51:38 +0200

    Added comments for Flow data discretization task

--- lab3/helper_functions/flow_data_discretization.py
+++ lab3/helper_functions/flow_data_discretization.py
@@ -15,12 +15,18 @@ import numpy as np
 #                     disc.append(i+1)
 #     return np.array(disc)
 
+'''
+This function performs ordinal discretization as done in https://ieeexplore.ieee.org/abstract/document/7987293.
+INPUT: val: values of the feature to be discretized,
+       ranks: number of ordinal ranks-percentiles used for the discretization
+       (e.g. 5 ranks, the 20,40,60,80 percentiles are considered, for value <=20 -> 0, 20<val<=40 -> 1, etc)
+OUTPUT: disc: discretized feature
+'''
 def discretize_ordinal(val, ranks):
 
     ordinal_ranks = np.linspace(0, 1, ranks, endpoint=False)
     ordinal_ind = np.ceil(ordinal_ranks[1:] * len(val))
     ordinal_val = np.array(sorted(val))[ordinal_ind.astype(int)]
-
 
     if len(ordinal_val)==1:
         disc = (val>ordinal_val).astype(int)
@@ -36,6 +42,13 @@ def discretize_ordinal(val, ranks):
     return np.array(disc)
 
 
+'''
+This function extract code values as done in https://ieeexplore.ieee.org/abstract/document/7987293, combining
+a set of features into a single value.
+INPUT: values: teh feature values from which codes will be extracted.
+OUTPUT: code: extracted code values,
+        M: spacesize (number of unique values) for each feature
+'''
 def extract_code(values):
     M = np.zeros(values.shape[1])
     for j in range(values.shape[1]):
@@ -48,4 +61,4 @@ def extract_code(values):
         for j in range(values.shape[1]):
             code[i] = code[i] + values[i,j] * s/M[j]
             s = s / M[j]
-    return code.astype(int), M
+    return code, M
