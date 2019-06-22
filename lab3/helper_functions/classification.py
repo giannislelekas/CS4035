6c1787e31f4f3e0c5c3854b4243f98d1f31cfdf0
ilelekas <giannislelekas@gmail.com>
2019-06-22 17:34:41 +0200

    Added comments for classification task

--- lab3/helper_functions/classification.py
+++ lab3/helper_functions/classification.py
@@ -7,65 +7,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve, auc, confusion_matrix, precision_score, recall_score, f1_score
 
 
-# Inspired by https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-# This function plots the confusion matrix.
-#
-# INPUT:
-#    cm        : confusion matrix to be printed (in form of a 2x2 matrix),
-#    classes   : the labels corresponding to the classes (in our case 'Settled' and 'Chargeback')
-#    normalize : whether to present normalized counts or not,
-#    title     : title of the provided plot,
-#    cmap      : the color map used for the visualization
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=classes, yticklabels=classes,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    return ax
-
-
 def performance_metrics(cm):
     accuracy = (cm[0,0] + cm[1,1])/np.sum(cm)
     precision = cm[0,0]/(cm[0,0]+cm[0,1])
@@ -115,13 +56,13 @@ def roc_analysis(X, y, clf, cv, plot_all_ROC, plot_ROC, attack=False, sm=None):
 
         X_test = X[test_index]
         feat = [0,4,5]
-        
+
         if attack:
             malicious_ind = np.where(y[test_index]==1)[0][..., np.newaxis]
             perturb = [120, 200, 2048]
             perturb = np.tile(perturb, [len(malicious_ind), 1])
             X_test[malicious_ind, feat] = X_test[malicious_ind, feat] + perturb
-        
+
         y_pred = clf.predict(X_test)
 
 
@@ -133,7 +74,7 @@ def roc_analysis(X, y, clf, cv, plot_all_ROC, plot_ROC, attack=False, sm=None):
         F1[i] = f1_score(y[test_index], y_pred, average='binary')
 
         # Extract false positive and true positive rate correspoding to the positive (fraud) label
-        fpr, tpr, thresholds = roc_curve(y[test_index], clf.predict_proba(X[test_index])[:, 1])
+        fpr, tpr, thresholds = roc_curve(y[test_index], clf.predict_proba(X_test)[:, 1])
 
         # Interpolate extracted values on range of 0:100 with a step of 1
         TPR.append(np.interp(mean_FPR, fpr, tpr))
